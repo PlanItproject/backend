f54a9aff5b94e2480ce044022b06652a7834a011
hyeonjune <annhj980@naver.com>
2025-03-04 12:48:51 +0900

    [FEAT] [travelplan] 기능 삭제

--- planit/src/main/java/com/trip/planit/travelplanner/repository/SelectedPlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trip.planit.travelplanner.repository;
-
-import com.trip.planit.travelplanner.model.SelectedPlace;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SelectedPlaceRepository extends JpaRepository<SelectedPlace, Long> {
-}
